db78e15eaaf2bc0712ed6e8d54efae3f3d8ad83e
Hossam Samir <hossamsamir@outlook.com>
2022-05-07 03:09:22 +0200

    property proxy

---
+++ Structural/Proxy/property_proxy.cpp
@@ -0,0 +1,29 @@
+// Property Proxy
+#include <iostream>
+
+template<typename T>
+struct Property {
+    T value;
+
+    Property(T value) { *this = value; }
+
+    operator T() { return value; }
+
+    T operator=(const T &rhs) {
+        std::cout << "Assignment " << std::endl;
+        return value = rhs;
+    }
+
+};
+
+struct Creature {
+    Property<int> strength{10};
+    Property<int> agility{5};
+};
+
+int main() {
+    Creature c;
+    c.strength = 10;
+    c.agility = 5;
+
+};
