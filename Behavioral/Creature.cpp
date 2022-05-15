31cdfb68b3f2d2c62e45a756a8c57f99cf153c7e
Hossam Samir <hossamsamir@outlook.com>
2022-05-15 10:55:14 +0200

    example for chain of responsibility

--- Behavioral/Creature.cpp
+++ Behavioral/Creature.cpp
@@ -1,5 +1,7 @@
 
 #include <string>
+#include <iostream>
+#include <ostream>
 
 using namespace std;
 
@@ -9,6 +11,13 @@ using namespace std;
 struct Creature {
     string name;
     int attack, defense;
+
+    Creature(const string &name, int attack, int defense) : name(name), attack(attack), defense(defense) {}
+
+    friend ostream &operator<<(ostream &os, const Creature &creature) {
+        os << "name: " << creature.name << " attack: " << creature.attack << " defense: " << creature.defense;
+        return os;
+    }
 };
 
 // Now, as the creature progresses through the game, it might come up to an item (e.g., a magic sword), or it might end
@@ -28,19 +37,60 @@ protected:
 public:
 
     void add(CreatureModifier *m) {
-        if (next)
+        if (next != nullptr)
             next->add(m);
         else
             next = m;
     }
-    virtual  void handle()
-    {
-        if(next)
+
+    virtual void handle() {
+        if (next)
             next->handle();
     }
-    CreatureModifier(Creature &creature) : creature(creature) {}
+
+    CreatureModifier(Creature &creature) : creature(creature) {
+        next = nullptr;
+    }
 };
 
 // So far, all we have is an implementation of append-only singly linked list.
 // But when we start inheriting from it, things will hopefully become clearer.
 // For example, here is how you would make a modifier that would double the creature’s attack value:
+
+struct DoubleAttackModifier : CreatureModifier {
+    DoubleAttackModifier(Creature &creature) : CreatureModifier(creature) {}
+
+    void handle() override {
+        creature.attack *= 2;
+        CreatureModifier::handle();
+    }
+};
+
+
+struct IncreaseDenseModifier : CreatureModifier {
+    IncreaseDenseModifier(Creature &creature) : CreatureModifier(creature) {}
+
+    void handle() override {
+        if (creature.attack <= 2) {
+            creature.defense += 1;
+            CreatureModifier::handle();
+        }
+    }
+};
+
+int main() {
+    Creature goblin{" goblin", 1, 1};
+    CreatureModifier root{goblin};
+    DoubleAttackModifier r1{goblin};
+    DoubleAttackModifier r2{goblin};
+    IncreaseDenseModifier r3{goblin};
+
+
+    root.add(&r1);
+    root.add(&r2);
+    root.add(&r3);
+
+    root.handle();
+
+    std::cout << goblin << std::endl;
+}
